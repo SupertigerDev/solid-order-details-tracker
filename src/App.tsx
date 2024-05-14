492412bac3c09e9cc69f9a7848aa4ee04e58b167
Supertiger <saawalkhn@googlemail.com>
2024-05-14 19:18:47 +0100

    Initial progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,78 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Show } from "solid-js"
 
 function App() {
-  const [count, setCount] = createSignal(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
+    <div class="bg-white border-gray-400 border-solid border rounded-lg p-6 w-[60vh]">
+      <MainDetails/>
+      <OrderDetails/>
+    </div>
+  )
+}
+
+
+const MainDetails = () => {
+  return (
+    <div class="flex">
+      <div class="border-gray-400 border rounded-lg w-28 h-28"></div>
+      <Badge/>
+
+    </div>
+  )
+}
+
+
+const Badge = () => {
+  return (
+    <div class="ml-auto bg-[#f8e6c2] rounded-full text-[#4d2f05] text-xs self-start px-3 py-1">
+      In progress
+    </div>
+  )
+}
+
+
+const OrderDetails = () => {
+  return (
+    <div class="mt-8">
+      <div class="font-bold text-3xl">Order #5913</div>
+      
+      <div class="flex flex-col gap-2 mt-4">
+        <div class="flex">
+          <Detail title="Item" value="Apple Watch"/>
+          <Detail title="Courier" value="UPS"/>
+        </div>
+        <div class="flex">
+          <Detail title="Start time" value="14 Jan 2024"/>
+          <Detail title="Address" value="4517 Washington Ave"/>
+        </div>
       </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div class="mt-6 flex gap-2">
+        <Button title="Export Details" primary />
+        <Button title="Request Confirmation" />
+        <Button iconSrc="more.svg" />
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+const Detail = (props: {title: string, value: string}) => {
+  return (
+    <div class="flex-1">
+      <div class="text-black/60">{props.title}</div>
+      <div>{props.value}</div>
+    </div>
+  )
+}
+
+const Button = (props: {title?: string, primary?: boolean, iconSrc?: string}) => {
+  return (
+    <button classList={{["bg-black text-white"]: props.primary, ["text-black border border-black/20"]: !props.primary}} class="rounded-lg py-2 px-4">
+      {props.title}
+      <Show when={props.iconSrc}>
+        <img class="w-2 h-2" src={props.iconSrc} />
+      </Show>
+    </button>
   )
 }
 
